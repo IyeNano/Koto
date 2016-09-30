fbd96b77d4b732f7defb2663fe18774ca3dad2ad
IyeNano <iyenano@gmail.com>
2016-09-30 11:17:10 -0500

    Another test to see if it transfers

---
+++ HelloKoto/src/Test2.java
@@ -0,0 +1,15 @@
+
+public class Test2 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int two1 = 2;
+		
+		System.out.println(two1);
+		
+	}
+
+}
