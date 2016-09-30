84d7916b7bc1d4d6b1562c71905b9867be9d4e7d
Jeremy Evert <jeremy.evert@swosu.edu>
2016-09-30 11:24:25 -0500

    added a new program and an outline. added by jeremy evert

---
+++ ConwayGameOfLife/src/ConwayGameOfLife.java
@@ -0,0 +1,23 @@
+
+public class ConwayGameOfLife {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		// declare variables
+		
+		// initilize world
+		
+		// set up a for loop 
+		
+		// go through and make the next the worldToCome
+		
+		// update display
+		
+		// copy the update and make Current World = worldToCome
+		
+		// after a thousand generations, show what the world was and what it has become.
+
+	}
+
+}
