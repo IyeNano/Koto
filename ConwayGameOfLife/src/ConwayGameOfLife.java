43863004d45fe4dfd0e05e0cda862a403dc026bf
Jeremy Evert <jeremy.evert@swosu.edu>
2016-09-30 11:47:20 -0500

    added a world and gave it life.

--- ConwayGameOfLife/src/ConwayGameOfLife.java
+++ ConwayGameOfLife/src/ConwayGameOfLife.java
@@ -2,22 +2,39 @@
 public class ConwayGameOfLife {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-		//For reference to "Conway's Game of Life: https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life"
-		
+		System.out.println("We are going to take over that \n" +
+				"world you are so fond of saying hello to...");
+
+		//For reference to "Conway's Game of Life: 
+		// https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life"
+
 		// declare variables
-		
+		int[][] gridWorld = new int[5][5];
+
+		double randomLifeStart = 0.0;
+
 		// initilize world
-		
+
+		for(int i = 0; i <5; i++){
+			for(int j = 0; j<5; j++){
+				randomLifeStart = Math.random();
+				if(0.3>randomLifeStart){
+					gridWorld[i][j] = 1;
+				}else{
+					gridWorld[i][j] = 0;
+				}
+			}
+		}
+		System.out.println("The world has life.");
+
 		// set up a for loop 
-		
+
 		// go through and make the next the worldToCome
-		
+
 		// update display
-		
+
 		// copy the update and make Current World = worldToCome
-		
+
 		// after a thousand generations, show what the world was and what it has become.
 
 	}
