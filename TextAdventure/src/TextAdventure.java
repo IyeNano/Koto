f69a2cfd1eab7a10c5bdb809d2447455bb1f69a4
IyeNano <iyenano@gmail.com>
2016-10-10 09:34:18 -0500

    Added a side project to work on to help learn effective story progression in programming.

---
+++ TextAdventure/src/TextAdventure.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class TextAdventure {
+	public static void main(String[] args) {
+		/* This is a text adventure meant to run off of a rolling system similar to 
+		 * Dungeons & Dragons. I will be utilizing rules from edition 3.5 just because
+		 * that is the version of D&D I am most familiar. This was inspired by Karamel's
+		 * Rock, Paper, Scissors Quest he made for Homework 3(? memory fuzzy). It's
+		 * going to run basic "Zork" progression as I hope not to railroad it nearly
+		 * as much.
+		 */
+		
+		System.out.println("Greetings, Adventurer! (Koto you are going to hate this project...)");
+
+	}
+
+}
