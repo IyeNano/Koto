d68306c2de3db2ffdfa5e223afa553adc08fbf77
IyeNano <iyenano@gmail.com>
2016-11-27 14:09:06 -0600

    Finished Character Creation

--- TextAdventure/src/TextAdventure.java
+++ TextAdventure/src/TextAdventure.java
@@ -19,6 +19,7 @@ public class TextAdventure {
 		int cha1 = (int)(Math.random()*6) + 10;
 		
 		
+		
 		//startIntroLoreAndCC custom method to establish Player Character
 		startIntroLoreAndCC(str1, dex1, con1, int1, wis1, cha1);
 	}
@@ -81,24 +82,22 @@ public class TextAdventure {
 				str1 = str1 - 2;
 			}
 			
+			
 			//Prompt user for Age and Gender
 			System.out.println("Are you male or female?");
 			
 			String playerGender = introInput.next();
 			
-			if ((playerGender.equalsIgnoreCase("male")) || 
-					(playerGender.equalsIgnoreCase("man")) || 
-					(playerGender.equalsIgnoreCase("guy")) || 
-					(playerGender.equalsIgnoreCase("dude")) || 
-					(playerGender.equalsIgnoreCase("bro"))) {
+			if (playerGender.equalsIgnoreCase("male")) {
 				System.out.println("You are a patriarch of your kin. A leader fallen from a large height.");
-			} else if ((playerGender.equalsIgnoreCase("female")) || 
-						(playerGender.equalsIgnoreCase("woman")) || 
-						(playerGender.equalsIgnoreCase("lady")) || 
-						(playerGender.equalsIgnoreCase("dudette")) || 
-						(playerGender.equalsIgnoreCase("madam"))) {
+				wis1 = wis1 - 1;
+				str1 = str1 + 1;
+			} else if (playerGender.equalsIgnoreCase("female")) {
 				System.out.println("You are a matriarch of your kin. A kindly teacher whose wisdom is long lost.");
+				con1 = con1 + 1;
+				str1 = str1 - 1;
 			}
+			
 			
 			System.out.println("Do you remember your age? (Enter Age):");
 			
@@ -112,17 +111,18 @@ public class TextAdventure {
 			int playerArchetype = introInput.nextInt();
 			
 			if (playerArchetype == 1) {
-				playerClassWarrior();
+				playerClassWarrior(playerName, str1, dex1, con1, int1, wis1, cha1);
 		} else if (playerArchetype == 2) {
-				playerClassRogue();
+				playerClassRogue(playerName, str1, dex1, con1, int1, wis1, cha1);
 		} else if (playerArchetype == 3) {
-				playerClassMage(); 
+				playerClassMage(playerName, str1, dex1, con1, int1, wis1, cha1); 
 				}
 		}
 		
 	}
 
-		private static void playerClassMage() {
+		private static void playerClassMage(String playerName, 
+				int str1, int dex1, int con1, int int1, int wis1, int cha1) {
 			//custom method for establishing a mage-based class
 			Scanner classInputMage = new Scanner(System.in);
 			System.out.println("You are a mage. Adept to the arcane arts and a manipulator of reality." +
@@ -132,20 +132,214 @@ public class TextAdventure {
 			int playerArchClass = classInputMage.nextInt();
 			
 			if (playerArchClass == 1) {
-				System.out.println("You are a conjuror of earth, mover of fire, and master of the elements.");
+				System.out.println("You are " + playerName + ". An elementalist; capable of conjuring earth," +
+						" dancing with fire, and mastering the elements.");
+				//Finalizing the stats for the character
+				int strFinal = str1;
+				int dexFinal = dex1;
+				int conFinal = con1;
+				int intFinal = int1 + 2;
+				int wisFinal = wis1;
+				int chaFinal = cha1;
+				
+				int statModifier;
+				
+				if (intFinal % 2 == 0){
+					statModifier = ((intFinal - 10) / 2);
+					charInformation(strFinal, dexFinal, conFinal, intFinal, wisFinal, chaFinal, statModifier);
+				} else 
+					statModifier = ((intFinal - 11) / 2);
+					charInformation(strFinal, dexFinal, conFinal, intFinal, wisFinal, chaFinal, statModifier);
+				
+			} else if (playerArchClass == 2) {
+				System.out.println("You are " + playerName + ". An illusionist; capable of manipulating the senses" +
+						"and warping the mind");
+				//Finalizing the stats for the character
+				int strFinal = str1;
+				int dexFinal = dex1;
+				int conFinal = con1;
+				int intFinal = int1;
+				int wisFinal = wis1 + 1;
+				int chaFinal = cha1 + 1;
+				
+				int statModifier;
+				
+				if (intFinal % 2 == 0){
+					statModifier = ((intFinal - 10) / 2);
+					charInformation(strFinal, dexFinal, conFinal, intFinal, wisFinal, chaFinal, statModifier);
+				} else 
+					statModifier = ((intFinal - 11) / 2);
+					charInformation(strFinal, dexFinal, conFinal, intFinal, wisFinal, chaFinal, statModifier);
+				
+			} else if (playerArchClass == 3) {
+				System.out.println("You are " + playerName + ". A scion of the dark; a master of a lost arts.");
+				//Finalizing the stats for the character
+				int strFinal = str1;
+				int dexFinal = dex1;
+				int conFinal = con1;
+				int intFinal = int1;
+				int wisFinal = wis1 + 2;
+				int chaFinal = cha1;
+				
+				int statModifier;
+				
+				if (intFinal % 2 == 0){
+					statModifier = ((intFinal - 10) / 2);
+					charInformation(strFinal, dexFinal, conFinal, intFinal, wisFinal, chaFinal, statModifier);
+				} else 
+					statModifier = ((intFinal - 11) / 2);
+					charInformation(strFinal, dexFinal, conFinal, intFinal, wisFinal, chaFinal, statModifier);
+				
 			}
-			
-		
 		}
 
-		private static void playerClassRogue() {
+		private static void playerClassRogue(String playerName, 
+				int str1, int dex1, int con1, int int1, int wis1, int cha1) {
 			//custom method for establishing a rogue-based class
-		
+			Scanner classInputRogue = new Scanner(System.in);
+			System.out.println("You are a rogue. A deceiver and a scoundral, but cunning." +
+								"\nWhat is your profession?");
+			System.out.println("Thief (1), Mesmer (2), Sellsword (3)");
+			
+			int playerArchClass = classInputRogue.nextInt();
+			
+			if (playerArchClass == 1) {
+				System.out.println("You are " + playerName + ". A worshipper of the coin and a controller of appraisal.");
+				//Finalizing the stats for the character
+				int strFinal = str1;
+				int dexFinal = dex1 + 1;
+				int conFinal = con1;
+				int intFinal = int1 + 1;
+				int wisFinal = wis1;
+				int chaFinal = cha1;
+				
+				int statModifier;
+				
+				if (dexFinal % 2 == 0){
+					statModifier = ((dexFinal - 10) / 2);
+					charInformation(strFinal, dexFinal, conFinal, intFinal, wisFinal, chaFinal, statModifier);
+				} else 
+					statModifier = ((dexFinal - 11) / 2);
+					charInformation(strFinal, dexFinal, conFinal, intFinal, wisFinal, chaFinal, statModifier);
+				
+			} else if (playerArchClass == 2) {
+				System.out.println("You are " + playerName + ". A mover of words and a speaker in dreams.");
+				//Finalizing the stats for the character
+				int strFinal = str1;
+				int dexFinal = dex1 + 1;
+				int conFinal = con1;
+				int intFinal = int1;
+				int wisFinal = wis1;
+				int chaFinal = cha1 + 1;
+				
+				int statModifier;
+				
+				if (dexFinal % 2 == 0){
+					statModifier = ((dexFinal - 10) / 2);
+					charInformation(strFinal, dexFinal, conFinal, intFinal, wisFinal, chaFinal, statModifier);
+				} else 
+					statModifier = ((dexFinal - 11) / 2);
+					charInformation(strFinal, dexFinal, conFinal, intFinal, wisFinal, chaFinal, statModifier);
+					
+			} else if (playerArchClass == 3) {
+				System.out.println("You are " + playerName + ". A kin to all blades who seek a darker path.");
+				//Finalizing the stats for the character
+				int strFinal = str1;
+				int dexFinal = dex1 + 2;
+				int conFinal = con1;
+				int intFinal = int1;
+				int wisFinal = wis1;
+				int chaFinal = cha1;
+				
+				int statModifier;
+				
+				if (dexFinal % 2 == 0){
+					statModifier = ((dexFinal - 10) / 2);
+					charInformation(strFinal, dexFinal, conFinal, intFinal, wisFinal, chaFinal, statModifier);
+				} else 
+					statModifier = ((dexFinal - 11) / 2);
+					charInformation(strFinal, dexFinal, conFinal, intFinal, wisFinal, chaFinal, statModifier);
+			}
 		}
 
-		private static void playerClassWarrior() {
+		private static void playerClassWarrior(String playerName, 
+				int str1, int dex1, int con1, int int1, int wis1, int cha1) {
 			//custom method for establishing a warrior-based class
-		
+			Scanner classInputWarrior = new Scanner(System.in);
+			System.out.println("You are a warrior. A hero of strength and a progenitor of might." +
+								"\nWhat is your honor?");
+			System.out.println("Guardian (1), Barbarian (2), Reaver (3)");
+			
+			int playerArchClass = classInputWarrior.nextInt();
+			
+			if (playerArchClass == 1) {
+				System.out.println("You are " + playerName + ". You are a protector of the innocent and a lawful upholder.");
+				//Finalizing the stats for the character
+				int strFinal = str1 + 1;
+				int dexFinal = dex1;
+				int conFinal = con1 + 1;
+				int intFinal = int1;
+				int wisFinal = wis1;
+				int chaFinal = cha1;
+				
+				int statModifier;
+				
+				if (strFinal % 2 == 0){
+					statModifier = ((strFinal - 10) / 2);
+					charInformation(strFinal, dexFinal, conFinal, intFinal, wisFinal, chaFinal, statModifier);
+				} else 
+					statModifier = ((strFinal - 11) / 2);
+					charInformation(strFinal, dexFinal, conFinal, intFinal, wisFinal, chaFinal, statModifier);
+				
+			} else if (playerArchClass == 2) {
+				System.out.println("You are " + playerName + ". You are a free spirit and a wrathful watcher.");
+				//Finalizing the stats for the character
+				int strFinal = str1 + 2;
+				int dexFinal = dex1;
+				int conFinal = con1;
+				int intFinal = int1;
+				int wisFinal = wis1;
+				int chaFinal = cha1;
+				
+				int statModifier;
+				
+				if (strFinal % 2 == 0){
+					statModifier = ((strFinal - 10) / 2);
+					charInformation(strFinal, dexFinal, conFinal, intFinal, wisFinal, chaFinal, statModifier);
+				} else 
+					statModifier = ((strFinal - 11) / 2);
+					charInformation(strFinal, dexFinal, conFinal, intFinal, wisFinal, chaFinal, statModifier);
+				
+			} else if (playerArchClass == 3) {
+				System.out.println("You are " + playerName + ". You are a skilled shot and a weary hunter.");
+				//Finalizing the stats for the character
+				int strFinal = str1 + 1;
+				int dexFinal = dex1 + 1;
+				int conFinal = con1;
+				int intFinal = int1;
+				int wisFinal = wis1;
+				int chaFinal = cha1;
+				
+				int statModifier;
+				
+				if (strFinal % 2 == 0){
+					statModifier = ((strFinal - 10) / 2);
+					charInformation(strFinal, dexFinal, conFinal, intFinal, wisFinal, chaFinal, statModifier);
+				} else 
+					statModifier = ((strFinal - 11) / 2);
+					charInformation(strFinal, dexFinal, conFinal, intFinal, wisFinal, chaFinal, statModifier);
+				
+				
+			}
 		}
-
+		private static void charInformation(int strFinal, int dexFinal, int conFinal, int intFinal, int wisFinal, int chaFinal, int statModifier) {
+			System.out.println("Your Strength Score is: " + strFinal);
+			System.out.println("Your Dexterity Score is: " + dexFinal);
+			System.out.println("Your Constitution Score is: " + conFinal);
+			System.out.println("Your Intelligence Score is: " + intFinal);
+			System.out.println("Your Wisdom Score is: " + wisFinal);
+			System.out.println("Your Charisma Score is: " + chaFinal);
+			System.out.println("Your Attack Modifier is: " + statModifier);
+		}
+		
 }
